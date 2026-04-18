fbdf8bf2326593540576779922800f60fdc0ac5f
kolagey308-glitch <kolagey308@gmail.com>
2026-04-18 19:03:46 +0300

    Update inline keyboard buttons in main_menu

--- bot.py
+++ bot.py
@@ -97,8 +97,9 @@ def main_menu():
     return {
         "inline_keyboard": [
             [
-                {"text": f"{emoji('5938413566624272793', '🛒')} Магазин", "callback_data": "shop_bot", "icon_custom_emoji_id": "5938413566624272793"},
-                {"text": f"{emoji('5879770735999717115', '👤')} Профиль", "callback_data": "profile", "icon_custom_emoji_id": "5879770735999717115"}
+
+                [{"text": "Магазин", "callback_data": "shop_bot", "icon_custom_emoji_id": "5294298833071674944"}],
+                [{"text": "Профиль", "callback_data": "profile", "icon_custom_emoji_id": "5294298833071674944"}]
             ],
             [
                 {"text": "🌐 Web Магазин", "web_app": {"url": WEBAPP_URL}}
